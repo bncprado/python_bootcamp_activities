798b3dcb6cfe129e62c90f1fc35ff62cffd500a5
bncprado <bncprado@gmail.com>
2023-03-09 18:22:59 +0000

    new file added

---
+++ 10. Lists 1.py
@@ -0,0 +1,17 @@
+favourite_websites = [
+    "Google",
+    "YouTube",
+    "Twitter"
+]
+
+favourite_websites.append("Google Maps")
+favourite_websites.append("Google Translate")
+
+favourite_websites.pop()
+
+"""This is a very efficient submission for this activity! You've used the correct syntax for the list; 
+your brackets are correct, and you've split all your list elements up with a comma. Great attention to detail!
+
+You've continued to demonstrate your confidence with dot notation with your list methods. 
+Using append twice is perfect - as it only takes one arguement, we can only use it to add one thing. 
+You've also demononstrated your underastanding of .pop() to remove the final item. Well done!"""
